e56a6d529418d3641e90652164d69b2f95a8d81c
ryza <fakhryzaa23@gmail.com>
2026-09-09 10:30:25 +0700

    feat(w03): implement gorouter navigation and riverpod state management for krs app

---
+++ poliwangi_akademikk_dashboard/android/app/src/main/kotlin/com/example/poliwangi_akademikk_dashboard/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.poliwangi_akademikk_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ poliwangi_akademikk_dashboard/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+// import 'package:poliwangi_akademikk_dashboard/modul_01/profile.dart';
+// import 'package:poliwangi_akademikk_dashboard/modul_02/profile.dart';
+import 'package:poliwangi_akademikk_dashboard/modul_03/modul_3_app.dart';
+
+// void main() {
+//   runApp(Modul03App());
+// }
+void main() {
+  runApp(Modul03App());
+}
+
